416f463d5b82378ca9b43e19bdff2c447860a27a
Billy Hao <billyhao12@gmail.com>
2020-08-22 16:58:21 -0700

    Built a template Project component. Added a server.js for heroku deployment.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
 import Footer from './components/Footer';
+import Project from './components/Project';
 
 function App() {
   return (
     <div>
       <Header />
+      <Project />
       <Footer />
     </div>
   );
