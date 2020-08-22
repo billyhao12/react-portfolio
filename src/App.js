9db307ee1b733b561ad9466a3c8e4eaca9061f1e
Billy Hao <billyhao12@gmail.com>
2020-08-21 20:46:23 -0700

    Built a Footer component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer>
+            &copy; Billy Hao
+        </footer>
+    )
+}
+
+export default Footer;
