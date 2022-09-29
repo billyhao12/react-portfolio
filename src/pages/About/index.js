28f49e9f13fbdc84d787440580f217ea220d99ca
Billy Hao <billyhao12@gmail.com>
2022-09-29 00:16:38 -0700

    Updated bio

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -24,7 +24,7 @@ function About() {
                         <Col lg={6}>
                             <p>
                                 Software engineer and problem solver working at Sinclair Digital.
-                                A graduate student at UW Bothell set to graduate with a master's degree in computer science and software engineering in 2025. Holds a Graduate Certificate in Software Design and Development (GCSDD) from the same university and a certificate in Full Stack Web Development from the University of Washington Professional and Continuing Education program. Earned a Bachelor of Arts in Business Administration from UW Bothell's School of Business with concentrations in Supply Chain Management and Finance in 2019.
+                                A graduate student at the University of Washington Bothell set to graduate with a master's degree in computer science and software engineering in 2025. Holds a Graduate Certificate in Software Design and Development (GCSDD) from the same university and a certificate in Full Stack Web Development from the UW Professional and Continuing Education program. Earned a Bachelor of Arts in Business Administration with concentrations in Supply Chain Management and Finance from UW Bothell's School of Business in 2019.
                             </p>
                             
                             <strong>Technical Skills</strong>
